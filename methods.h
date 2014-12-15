ceed2ae37106ef904f93d20302ec352d47bdfd9e
jhlr <jhlrietra@gmail.com>
2014-12-15 21:33:34 +0000

    Update methods.h

--- methods.h
+++ methods.h
@@ -5,11 +5,9 @@
 	typedef struct class_name class_name;\
 	class_name class_name ## _default;\
 	struct class_name 
-#define VA1(...)\
-	, ## __VA_ARGS__
 #define method(class_name, meth_name, args...)\
-	_ ## class_name ## _ ## meth_name (class_name *self VA1(args))
+	_ ## class_name ## _ ## meth_name (class_name *self , ## args)
 #define $(inst, meth, args...)\
-	inst.meth(&inst VA1(args))
+	inst.meth(&inst , ## args)
 
 #endif
